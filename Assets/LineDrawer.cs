5b69610f69507c8dddd7a2903445a30105ee8f45
DESKTOP-2KM365Q\Mustafa <metro9952@gmail.com>
2020-11-19 19:19:59 +0400

    First Drawing Functionality added

--- Assets/LineDrawer.cs
+++ Assets/LineDrawer.cs
@@ -4,39 +4,78 @@ using UnityEngine;
 
 public class LineDrawer : MonoBehaviour
 {
-    public GameObject[] objects;
-    private Vector3[] linePositions; 
+    private int clicksPositionsCount;
+    private List<Vector3> clicksPositions;
     private LineRenderer lineRenderer;
+    private bool hasTouched;
     void Start()
     {
+        clicksPositionsCount = 0;
+        clicksPositions = new List<Vector3>();
         lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.widthMultiplier = 0.2f;
-        linePositions = new Vector3[objects.Length];
-        DrawLine();
     }
 
     void Update()
     {
-    
+        TouchDetector();
+        print(hasTouched);
+        RegisterclicksPositions();
+        DrawLineByTouch();
     }
 
-    void GetLinePositions()
+    void DrawLineByTouch()
     {
-        //Getting positions from drawing
+        if (hasTouched)
+        {
+            Touch[] touches = Input.touches;
+            Vector3[] touchesVector = new Vector3[Input.touchCount];
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                touchesVector[i] = new Vector3(touches[i].position.x, touches[i].position.y, 0);
+            }
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                lineRenderer.positionCount = Input.touchCount;
+                lineRenderer.SetPositions(touchesVector);
+            }
+
+#if UNITY_EDITOR
+            for (int i = 0; i < clicksPositionsCount; i++)
+            {
+                lineRenderer.positionCount = clicksPositionsCount;
+                lineRenderer.SetPositions(clicksPositions.ToArray());
+                print("YAY");
+
+            }
+#endif
+        }
     }
 
-    void DrawLine()
-    {
-        
-        for(int i = 0; i < objects.Length; i++)
+    void RegisterclicksPositions()
+    {   
+        if (hasTouched)
         {
-            linePositions[i] = objects[i].transform.position;
-        }   
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            clicksPositionsCount++;
 
-        for(int i = 0; i < objects.Length; i++)
+            clicksPositions.Add(new Vector3(position.x, position.y, 0));
+        }
+    }
+
+    void TouchDetector()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
-            lineRenderer.positionCount = objects.Length;
-            lineRenderer.SetPositions(linePositions);
-        }  
+            hasTouched = true;
+        }
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasTouched = true;
+        }
+#endif
+
+        hasTouched = false;
     }
 }
---
+++ Assets/TestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDraw : MonoBehaviour
+{
+    public GameObject LinePrefab;
+    public GameObject currentLine;
+
+    public LineRenderer lineRenderer;
+    public List<Vector2> touchPositions;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+        }
+    }
+
+    void CreateLine()
+    {
+        currentLine = Instantiate(LinePrefab, Vector3.zero, Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        touchPositions.Clear();
+        touchPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        touchPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        lineRenderer.SetPosition(0, touchPositions[0]);
+        lineRenderer.SetPosition(1, touchPositions[1]);
+    }
+}
